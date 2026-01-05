2c97a9e677dc2c700160bd25321bf74ac60ddfeb
Than-sothearak <thearak643@gmail.com>
2026-01-05 17:48:36 +0700

    fixed scholl

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -21,7 +21,7 @@ export const Contact = () => {
         process.env.NEXT_PUBLIC_SERVICE_ID,
         process.env.NEXT_PUBLIC_TEMPLATE_ID,
         form.current,
-        process.env.NEXT_PUBLIC_PUBLIC_KEY
+        process.env.NEXT_PUBLIC_PUBLIC
       )
       .then(
         () => {
